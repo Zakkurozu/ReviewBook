09011e8259d7aed3c3ce48a126e88b7a855da76a
Zakkurozu <afrizalahma5@gmail.com>
2024-02-15 16:35:50 +0700

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["src/**/*.{html, js}"],
+  content: ["index.html"],
   theme: {
     container:{
       center: true,
@@ -30,4 +30,3 @@ module.exports = {
     require('tailwind-scrollbar-hide'),
   ],
 }
-
